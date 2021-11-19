6a08a7895dcfa2b7d98956b0246f58c2402245d9
Arjuna Rajasekhar <arjunar117@gmail.com>
2021-11-19 15:02:02 -0500

    initial version

---
+++ VirtualKeyProject/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+import java.io.*;
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Virtual Key App: developed by Arjuna");
+        System.out.println("You can View, create & delete files at your discretion");
+
+        start.menu();
+
+
+    }
+}
---
+++ VirtualKeyProject/src/com/company/start.java
@@ -0,0 +1,93 @@
+package com.company;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class start {
+    public static void menu() {
+        File ob = new File("KeyFolder");
+        ob.mkdir();
+        File array[] = ob.listFiles();
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Main menu:");
+        System.out.println("Press 1 to view files in the main folder");
+        System.out.println("Press 2 to Edit files in the main folder");
+        System.out.println("Press 3 to exit the program");
+        boolean flag = true;
+        while (flag) {
+            int switchValue1 = scan.nextInt();
+            switch (switchValue1) {
+                case 1:
+                    for (int i = 0; i < array.length; i++) {
+                        System.out.println(array[i]);
+                    }
+                    break;
+                case 2:
+                    System.out.println("File editing menu: ");
+                    System.out.println("Press 1 to ADD file ");
+                    System.out.println("Press 2 to DELETE a file ");
+                    System.out.println("Press 2 to SEARCH for a file ");
+                    System.out.println("Press 4 to return to the main menu ");
+                    System.out.println("Press 5 to exit the program");
+                    boolean flag2 = true;
+                    while (flag2) {
+                        int switchValue2 = scan.nextInt();
+                        Scanner scan2 = new Scanner(System.in);
+                        switch (switchValue2) {
+                            case 1:
+                                System.out.println("Please enter the name of the new File you want to add: ");
+                                String newName = scan2.nextLine();
+                                File folder = new File("KeyFolder\\"+newName);
+                                folder.mkdirs();
+                                System.out.println("File added, press 1 to add another");
+
+                                break;
+                            case 2:
+                                System.out.println("Please enter the name of the file you want to delete: ");
+                                String name = scan2.nextLine();
+                                File delete = new File("KeyFolder\\"+name);
+                                delete.delete();
+
+                                break;
+                            case 3:
+                                System.out.println("Please enter the name of the file: ");
+                                String name2 = scan2.nextLine();
+                                for (int i = 0; i < array.length; i++) {
+                                    if (array[i].getName().startsWith(name2)) {
+                                        System.out.println(array[i]);
+                                    }
+                                }
+                                break;
+                            case 4:
+                                System.out.println("Returning to main menu");
+                                flag2 = false;
+                                System.out.println("Main menu:");
+                                System.out.println("Press 1 to view files in the main folder");
+                                System.out.println("Press 2 to Edit files in the main folder");
+                                System.out.println("Press 3 to exit the program");
+                                break;
+                            case 5:
+                                System.out.println("Exiting program");
+                                flag = false;
+                                flag2 = false;
+                                break;
+                            default:
+                                System.out.println("Please select a valid option");
+                                break;
+                        }
+                    }
+                    break;
+
+                case 3:
+                    System.out.println("Exiting Program");
+                    flag = false;
+                    break;
+
+                default:
+                    System.out.println("Please enter a valid option");
+                    break;
+            }
+        }
+    }
+}
